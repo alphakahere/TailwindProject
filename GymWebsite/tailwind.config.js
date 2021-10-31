82d89d4f22d0ae6ec57db84e222e4342540d2e08
alphakahere <alphakahere@gmail.com>
2021-10-31 22:50:59 +0000

    initial commit

---
+++ GymWebsite/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        orange:'#f07115',
+        orangefort: '#f96b04'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ TailwindKit/tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        beise: "##F1F5F9",
+        blueGray: {
+          500 : "#3c5270",
+          600 : "#475569"
+        }, 
+        orange: {
+          500: "#f07115"
+        },
+        blue: {
+          69 : "#27A5E9"
+        },
+        green:{
+          perso: "#5DBB81"
+        }, 
+        blueGray: '#475569'
+      },
+      minHeight: {
+        hs:'155vh',
+        hm: '110vh'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
